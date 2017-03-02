dff34f4ec7cceb7ac4181d32c22fab0ce4efe4e2
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-02 11:59:42 -0500

    Move protobuf logic to protob.rs

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,15 @@
 extern crate protobuf;
 
 mod glob;
-
-use std::fs::*;
-use std::io::Read;
-use std::io::Write;
-use std::path::Path;
-
-use protobuf::parse_from_reader;
-use protobuf::descriptor::*;
-use protobuf::codegen::*;
-
-fn write_file(bin: &str) {
-    let mut is = File::open(&Path::new(bin)).unwrap();
-    let fds = parse_from_reader::<FileDescriptorSet>(&mut is as &mut Read).unwrap();
-
-    let file_names: Vec<String> = fds.get_file().iter()
-        .map(|f| f.get_name().to_string())
-        .collect();
-    let results = gen(fds.get_file(), &file_names);
-
-    for r in &results {
-        let mut file_writer = File::create(&Path::new(&r.name)).unwrap();
-        file_writer.write(&r.content).unwrap();
-    }
-}
+mod protob;
 
 fn main() {
     let fdset_files = glob::discover_fdset_files();
 
-    for f in fdset_files {
-        println!("{}", f.display());
+    for file in fdset_files {
+        let file_str = file.to_string_lossy();
+        protob::write_file(&file_str);
     }
-
     /*
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 2 {
