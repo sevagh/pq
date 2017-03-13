2552d3d33d0003693d30fd000c09d431593410f1
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-13 10:50:39 -0400

    Add make targets: build/clean/test

--- make.sh
+++ make.sh
@@ -14,11 +14,16 @@ function gen_schemata() {
     protoc --rust_out ./src/schemata ./schemata/*.proto
 }
 
-function run_cargo() {
+if [ z"${1-}" == z"build" ]; then
+    clean_gen_code
+    gen_schemata
     touch src/main.rs
     cargo build
-}
-
-clean_gen_code
-gen_schemata
-run_cargo
+elif [ z"${1-}" == z"clean" ]; then
+    clean_gen_code
+elif [ z"${1-}" == z"test" ]; then
+    cargo test
+else
+    printf "usage: $0 build|clean|test\n" $0
+    exit 255
+fi
