9d10d70c7acef6fad34f5d53c92a2a4aa58bac4b
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-30 13:25:12 -0400

    Apparently it's impossible to decode without fdsets

--- src/protob.rs
+++ src/protob.rs
@@ -1,23 +1,18 @@
+use std::env;
+use std::fs::File;
 use std::io::Read;
 use serde::de::Deserialize;
 use serde_protobuf::descriptor::Descriptors;
 use serde_protobuf::de::Deserializer;
-use serde_protobuf::value::Message;
 use serde_value::Value;
 use protobuf::{CodedInputStream, parse_from_reader};
 
-static UNKNOWN_FDSET: &'static [u8] = b"
-*
-\runknown.protoxyz.sevag.pqrs\"	
-Unknown";
-
 pub fn process_single(read: &mut Read) {
-    let proto = parse_from_reader(&mut UNKNOWN_FDSET.clone()).unwrap();
+    let proto = parse_from_reader(&mut open_combined_fdset()).unwrap();
     let descriptors = Descriptors::from_proto(&proto);
-
     let byte_is = CodedInputStream::new(read);
 
-    let mut deserializer = Deserializer::for_named_message(&descriptors, ".xyz.sevag.pqrs.Unknown", byte_is).unwrap();
+    let mut deserializer = Deserializer::for_named_message(&descriptors, ".com.example.dog.Dog", byte_is).unwrap();
     let value = Value::deserialize(&mut deserializer).unwrap();
     println!("{:?}", value);
 }
@@ -32,5 +27,19 @@ pub fn process_stream(read: &mut Read) {
             Ok(false) => break, 
             //todo: actually do deserialization here
         }
+    }
+}
+
+fn open_combined_fdset() -> File {
+    let mut home = match env::home_dir() {
+        Some(home) => home,
+        None => panic!("Could not find $HOME"),
+    };
+
+    home.push(".pq/combined.fdset");
+
+    match File::open(home.as_path()) {
+        Ok(x) => return x,
+        Err(e) => panic!(e),
     }
 }
