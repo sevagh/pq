480dca3c8dc2e252ad6fb583dcebbe712c5eb08e
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-30 05:40:56 -0400

    Compiles now

--- src/protob.rs
+++ src/protob.rs
@@ -9,19 +9,18 @@ use self::serde::de::Deserialize;
 use self::serde_protobuf::descriptor::Descriptors;
 use self::serde_protobuf::de::Deserializer;
 use self::serde_value::Value;
+use self::protobuf::{CodedInputStream, parse_from_reader};
 
 pub fn process_single(read: &mut Read) {
+    let mut file = fs::File::open("testdata/address.fdset").unwrap();
+    let proto = parse_from_reader(&mut file).unwrap();
+    let descriptors = Descriptors::from_proto(&proto);
+
     let mut buffer = Vec::new();
     read.read(&mut buffer).unwrap();
+    let mut byte_is = CodedInputStream::from_bytes(&buffer);
 
-    let mut byte_is = protobuf::CodedInputStream::from_bytes(&buffer);
-
-    let mut file = fs::File::open("testdata/address.fdset").unwrap();
-
-    let proto = protobuf::parse_from_reader(&mut file).unwrap();
-
-    let descriptors = Descriptors::from_proto(&proto);
-    let mut deserializer = Deserializer::for_named_message(&descriptors, "pq-rs", byte_is).unwrap();
+    let mut deserializer = Deserializer::for_named_message(&descriptors, ".google.protobuf.FileDescriptorSet", byte_is).unwrap();
     let value = Value::deserialize(&mut deserializer).unwrap();
     println!("{:?}", value);
 }
