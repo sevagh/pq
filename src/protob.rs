f0538585b5eba06be5da5d11441fb6b3bb841884
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-30 14:41:00 -0400

    Try to maintain message names per fdset

--- src/protob.rs
+++ src/protob.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::{File, read_dir};
 use std::io::Read;
+use std::string::string;
 use serde::de::Deserialize;
 use serde_protobuf::descriptor::Descriptors;
 use serde_protobuf::de::Deserializer;
@@ -8,7 +9,7 @@ use serde_value::Value;
 use protobuf::{CodedInputStream, parse_from_reader};
 
 pub fn process_single(read: &mut Read) {
-    for mut fdset_file in discover_fdsets() {
+    for mut fdset_file, mut named_message in discover_fdsets() {
         let proto = parse_from_reader(&mut fdset_file).unwrap();
         let descriptors = Descriptors::from_proto(&proto);
         let byte_is = CodedInputStream::new(read);
@@ -32,7 +33,7 @@ pub fn process_stream(read: &mut Read) {
     }
 }
 
-fn discover_fdsets() -> Vec<File> {
+fn discover_fdsets() -> Vec<(File, String)> {
     let mut fdset_files = Vec::new();
 
     let mut home = env::home_dir().expect("Could not find $HOME");
@@ -40,6 +41,7 @@ fn discover_fdsets() -> Vec<File> {
     let paths = read_dir(home.as_path()).unwrap();
 
     for p in paths {
+        let mut named_messages = Vec::new();
         let path = match p {
             Ok(p) => p.path(),
             Err(_) => continue,
@@ -47,12 +49,19 @@ fn discover_fdsets() -> Vec<File> {
         match path.extension() {
             Some(x) => {
                 if x != "fdset" {
+                    if x == "names" {
+                        let mut f = File::open(path).unwrap();
+                        for l in f.lines() {
+                            named_messages.push(l);
+                        }
+                    }
                     continue;
                 }
             },
             None => continue,
         }
-        fdset_files.push(File::open(path.as_path()).unwrap());
+        fdset_files.push((File::open(path.as_path()).unwrap(),
+                          named_messages.first());
     }
     return fdset_files;
 }
