64f082ded7104d62689e6b27e1c376c84a8512a7
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-03-31 11:11:27 -0400

    Discover all MessageDescriptors per fdset

--- src/protob.rs
+++ src/protob.rs
@@ -3,27 +3,39 @@ use std::fs::{File, read_dir};
 use std::io::Read;
 use std::path::PathBuf;
 use serde::de::Deserialize;
-use serde_protobuf::descriptor::Descriptors;
+use serde_protobuf::descriptor::{Descriptors, MessageDescriptor};
 use serde_protobuf::de::Deserializer;
 use serde_value::Value;
 use protobuf::{CodedInputStream, parse_from_reader};
+use protobuf::descriptor::{FileDescriptorSet, FileDescriptorProto};
 
 pub fn process_single(read: &mut Read) {
     let mut descriptors = Descriptors::new();
+    let mut message_descriptors = Vec::new();
 
     for fdset_path in discover_fdsets() {
         let mut fdset_file = File::open(fdset_path.as_path()).unwrap();
-        let proto = parse_from_reader(&mut fdset_file).unwrap();
-        descriptors.add_file_set_proto(&proto);
+        let fdset: FileDescriptorSet = parse_from_reader(&mut fdset_file).unwrap();
+        descriptors.add_file_set_proto(&fdset);
+        let fdset_protos: &[FileDescriptorProto] = fdset.get_file();
+        for file_proto in fdset_protos.iter() {
+            for message_proto in file_proto.get_message_type().iter() {
+                println!("{:?}", message_proto);
+                message_descriptors.push(MessageDescriptor::from_proto(&fdset_path.to_string_lossy().into_owned().as_str(), message_proto));
+            }
+        }
     }
 
     descriptors.resolve_refs();
 
     let byte_is = CodedInputStream::new(read);
 
-    let mut deserializer = Deserializer::for_named_message(&descriptors, ".com.example.dog.Dog", byte_is).unwrap();
-    let value = Value::deserialize(&mut deserializer).unwrap();
-    println!("{:?}", value);
+    for md in message_descriptors {
+        println!("ATTEMPT TO DESERIALIE WITH: {:?}\n\n", md);
+        let mut deserializer = Deserializer::new(&descriptors, &md, byte_is);
+        let value = Value::deserialize(&mut deserializer).unwrap();
+        println!("{:?}", value);
+    }
 }
 
 pub fn process_stream(read: &mut Read) {
@@ -61,5 +73,5 @@ fn discover_fdsets() -> Vec<PathBuf> {
         }
         fdset_files.push(path);
     }
-    return fdset_files;
+    fdset_files
 }
