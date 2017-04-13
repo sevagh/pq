9c4663364fbb413d2a519afde9987fa8647c1a35
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-04-13 05:22:21 -0400

    Add stdin tests

--- tests/runner/mod.rs
+++ tests/runner/mod.rs
@@ -2,11 +2,12 @@
 
 use std::env;
 use std::path::PathBuf;
-use std::process;
+use std::process::{Command, Output, Child, Stdio};
 
 pub struct Runner {
-    pub cmd: process::Command,
+    pub cmd: Command,
     pub tests_path: PathBuf,
+    pub chld: Option<Child>,
 }
 
 impl Runner {
@@ -21,14 +22,22 @@ impl Runner {
         }
         let mut tests_path = root.parent().unwrap().parent().unwrap().to_path_buf();
         tests_path.push("tests");
-        let cmd = process::Command::new(root.join("pq"));
+        let mut cmd = Command::new(root.join("pq"));
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
         Runner {
             cmd: cmd,
             tests_path: tests_path,
+            chld: None,
         }
     }
 
-    pub fn run(&mut self) -> process::Output {
-        self.cmd.output().unwrap()
+    pub fn spawn(&mut self) {
+        let chld = self.cmd.spawn().unwrap();
+        self.chld = Some(chld);
+    }
+
+    pub fn output(&mut self) -> Output {
+        self.chld.take().unwrap().wait_with_output().unwrap()
     }
 }
--- tests/test_pqrs_bin.rs
+++ tests/test_pqrs_bin.rs
@@ -2,37 +2,62 @@ extern crate protobuf;
 
 mod runner;
 
-use std::process;
+use std::process::{Stdio, Output};
+use std::io::Write;
 use runner::Runner;
 
 fn for_nonexistent_file(work: &mut Runner) {
     work.cmd.arg("file-doesnt-exist");
+    work.spawn();
 }
 
 fn for_dog(work: &mut Runner) {
     work.cmd.arg(&work.tests_path.join("samples/dog"));
+    work.spawn();
 }
 
 fn for_person(work: &mut Runner) {
     work.cmd.arg(&work.tests_path.join("samples/person"));
+    work.spawn();
 }
 
-fn run_pqrs<F>(modify_arg: F) -> process::Output
-    where F: FnOnce(&mut Runner)
+fn with_stdin(work: &mut Runner) {
+    work.cmd.stdin(Stdio::piped());
+    work.spawn();
+    work.chld
+        .take()
+        .unwrap()
+        .stdin
+        .unwrap()
+        .write_all(b"")
+        .unwrap();
+}
+
+fn empty_stdin(work: &mut Runner) {
+    work.spawn();
+}
+
+fn run_pqrs<Farg, Fstdin>(modify_arg: Farg, modify_stdin: Fstdin) -> Output
+    where Farg: FnOnce(&mut Runner),
+          Fstdin: FnOnce(&mut Runner)
 {
     let mut work = Runner::new();
 
     work.cmd
         .arg("--fdsets")
         .arg(&work.tests_path.join("fdsets"));
-    modify_arg(&mut work);
 
-    work.run()
+    modify_arg(&mut work);
+    modify_stdin(&mut work);
+
+    work.output()
 }
 
 #[test]
 fn test_dog_decode() {
-    let out = run_pqrs(for_dog);
+    let out = run_pqrs(for_dog, empty_stdin);
+
+    println!("DEBUG: {:?}", out);
 
     //check if success
     assert!(out.status.success());
@@ -44,7 +69,7 @@ fn test_dog_decode() {
 
 #[test]
 fn test_person_decode() {
-    let out = run_pqrs(for_person);
+    let out = run_pqrs(for_person, empty_stdin);
 
     //check if success
     assert!(out.status.success());
@@ -56,7 +81,7 @@ fn test_person_decode() {
 
 #[test]
 fn test_nonexistent_file() {
-    let out = run_pqrs(for_nonexistent_file);
+    let out = run_pqrs(for_nonexistent_file, empty_stdin);
 
     //check if success
     assert_eq!(out.status.code().unwrap(), 255);
