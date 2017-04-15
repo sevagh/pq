b50b0c71ad2fdfbf8e2a32907917338255890b5f
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-04-14 20:26:21 -0400

    Better code style in testbench.py

--- py-test/testbench.py
+++ py-test/testbench.py
@@ -8,37 +8,34 @@ from google.protobuf.internal import encoder
 
 
 def gen_person():
-    person = person_pb2.Person()
-    person.id = random.choice(range(0, 4))
-    names = ['raffi', 'khosrov', 'vahaken']
-    person.name = names[random.choice(range(0, 3))]
+    person = person_pb2.Person(id=random.choice(range(0, 4)),
+                               name=['raffi', 'khosrov', 'vahaken']
+                               [random.choice(range(0, 3))])
     return person.SerializeToString()
 
 
 def gen_dog():
-    dog = dog_pb2.Dog()
-    dog.age = random.choice(range(0, 20))
-    breeds = ['rottweiler', 'gsd', 'poodle']
-    temperaments = ['chill', 'aggressive', 'excited']
-    dog.breed = breeds[random.choice(range(0, 3))]
-    dog.temperament = temperaments[random.choice(range(0, 3))]
+    dog = dog_pb2.Dog(age=random.choice(range(0, 20)),
+                      breed=['rottweiler', 'gsd', 'poodle']
+                      [random.choice(range(0, 3))],
+                      temperament=['chill', 'aggressive', 'excited']
+                      [random.choice(range(0, 3))])
     return dog.SerializeToString()
 
 
 if __name__ == '__main__':
     gen_funcs = [gen_person, gen_dog]
     if len(sys.argv) != 2:
-        print('Usage: {0} stream|single|dirty'.format(sys.argv[0]),
-              file=sys.stderr)
+        print('Usage: {0} stream|single|dirty'.format(sys.argv[0]))
         sys.exit(255)
     if sys.argv[1] == 'stream':
         while True:
             obj = gen_funcs[random.choice(range(0, 2))]()
-            delimiter = encoder._VarintBytes(len(obj))
-            sys.stdout.buffer.write(delimiter + obj)
+            sys.stdout.buffer.write(encoder._VarintBytes(len(obj)) +
+                                    obj)
     elif sys.argv[1] == 'single':
         sys.stdout.buffer.write(gen_funcs[random.choice(range(0, 2))]())
     elif sys.argv[1] == 'dirty':
-        sys.stdout.buffer.write(b'abcd')
-        sys.stdout.buffer.write(gen_funcs[random.choice(range(0, 2))]())
-        sys.stdout.buffer.write(b'\n')
+        sys.stdout.buffer.write([b'1zk&', b'ksfU', b'^M^M']
+                                [random.choice(range(0, 3))] +
+                                gen_funcs[random.choice(range(0, 2))]() + b'\n')
