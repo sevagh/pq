441664cae2abfe4bbbec9f952063628b45c18ccc
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-09 23:55:49 -0400

    Upgrade serde (#47)
    * Upgrade to serde 1.0

--- src/newline_pretty_formatter/mod.rs
+++ src/newline_pretty_formatter/mod.rs
@@ -1,6 +1,5 @@
 use serde_json::ser::{Formatter, PrettyFormatter};
-use serde_json::error::{Result, Error};
-use std::io::Write;
+use std::io::{self, Write};
 
 #[derive(Default)]
 pub struct NewlineFormatter {
@@ -9,42 +8,39 @@ pub struct NewlineFormatter {
 }
 
 impl Formatter for NewlineFormatter {
-    fn begin_array<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn begin_array<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.begin_array(w)
     }
-    fn end_array<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn end_array<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.end_array(w)
     }
-    fn begin_array_value<W: ?Sized + Write>(&mut self, w: &mut W, first: bool) -> Result<()> {
+    fn begin_array_value<W: ?Sized + Write>(&mut self, w: &mut W, first: bool) -> io::Result<()> {
         self.pretty.begin_array_value(w, first)
     }
-    fn end_array_value<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn end_array_value<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.end_array_value(w)
     }
-    fn begin_object<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn begin_object<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.depth += 1;
         self.pretty.begin_object(w)
     }
-    fn end_object<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn end_object<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.end_object(w).and_then(|()| {
             self.depth -= 1;
             if self.depth == 0 {
-                match w.write_all(b"\n") {
-                    Ok(_) => Ok(()),
-                    Err(e) => Err(Error::from(e)),
-                }
+                w.write_all(b"\n")
             } else {
                 Ok(())
             }
         })
     }
-    fn begin_object_key<W: ?Sized + Write>(&mut self, w: &mut W, first: bool) -> Result<()> {
+    fn begin_object_key<W: ?Sized + Write>(&mut self, w: &mut W, first: bool) -> io::Result<()> {
         self.pretty.begin_object_key(w, first)
     }
-    fn begin_object_value<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn begin_object_value<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.begin_object_value(w)
     }
-    fn end_object_value<W: ?Sized + Write>(&mut self, w: &mut W) -> Result<()> {
+    fn end_object_value<W: ?Sized + Write>(&mut self, w: &mut W) -> io::Result<()> {
         self.pretty.end_object_value(w)
     }
 }
