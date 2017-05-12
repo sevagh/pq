c0f7d12045cc0896fae9e264d8f475d8aee1ca65
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-05-12 11:01:13 -0400

    Add count param

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,8 @@ const USAGE: &'static str = "
 pq - protobuf to json
 
 Usage:
-  pq <infile> [--msgtype=<msgtype>] [--stream=<delim>]
-  pq kafka <topic> --brokers=<brokers> [--from-beginning]
+  pq <infile> [--msgtype=<msgtype>] [--stream=<delim>] [--count=<count>]
+  pq kafka <topic> --brokers=<brokers> [--from-beginning] [--count=<count>]
   pq (--help | --version)
 
 Options:
@@ -40,6 +40,7 @@ Options:
   --msgtype=<msgtype>   Message type e.g. com.example.Type
   --brokers=<brokers>   1.2.3.4:9092,5.6.7.8:9092
   --from-beginning      Consume kafka from beginning
+  --count=<count>       Stop after count messages
   --help                Show this screen.
   --version             Show version.
 ";
@@ -50,6 +51,7 @@ struct Args {
     pub arg_infile: Option<String>,
     pub arg_topic: Option<String>,
     pub flag_msgtype: Option<String>,
+    pub flag_count: Option<i32>,
     pub flag_stream: Option<String>,
     pub flag_from_beginning: bool,
     pub flag_brokers: Option<String>,
@@ -75,7 +77,14 @@ fn main() {
         if let (Some(brokers), Some(topic)) = (args.flag_brokers, args.arg_topic) {
             match StreamDelimiter::for_kafka(&brokers, &topic, args.flag_from_beginning) {
                 Ok(delim) => {
+                    let mut ctr = 0;
                     for chunk in delim {
+                        if let Some(count) = args.flag_count {
+                            if ctr >= count {
+                                process::exit(0);
+                            }
+                        }
+                        ctr += 1;
                         match pqrs_decoder.decode_message(&chunk, &mut stdout.lock(), out_is_tty) {
                             Ok(_) => (),
                             Err(e) => errexit!(e),
@@ -109,7 +118,14 @@ fn main() {
 
         if let Some(lead_delim) = args.flag_stream {
             let delim = StreamDelimiter::new(&lead_delim, &mut infile);
+            let mut ctr = 0;
             for chunk in delim {
+                if let Some(count) = args.flag_count {
+                    if ctr >= count {
+                        process::exit(0);
+                    }
+                }
+                ctr += 1;
                 match pqrs_decoder.decode_message(&chunk, &mut stdout.lock(), out_is_tty) {
                     Ok(_) => (),
                     Err(e) => errexit!(e),
