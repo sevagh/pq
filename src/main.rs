4053f079f0076e20371e4f124c5abe60fb720d3d
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-06-02 10:24:43 -0400

    Delegate argparsing to subcommand fns

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,12 @@ mod decode;
 use std::fs::File;
 use decode::PqrsDecoder;
 use stream_delimit::stream_consumer::StreamConsumer;
-use stream_delimit::stream_type::{string_to_stream_type, StreamType};
+use stream_delimit::stream_type::string_to_stream_type;
 use stream_delimit::stream_converter::StreamConverter;
 use std::io::{self, Read, BufReader, Write, stderr};
 use std::process;
 use error::PqrsError;
+use clap::ArgMatches;
 
 macro_rules! errexit {
     ($error:expr) => ({
@@ -48,44 +49,18 @@ fn main() {
     )
             .get_matches();
 
-    let pqrs_decoder = match PqrsDecoder::new(matches.value_of("MSGTYPE")) {
-        Ok(x) => x,
-        Err(e) => errexit!(e),
-    };
-
-    let count = value_t!(matches, "COUNT", i32).unwrap_or(-1);
-    let convert = matches.value_of("CONVERT");
-
     match matches.subcommand() {
-        ("kafka", Some(m)) => {
-            run_kafka(pqrs_decoder,
-                      m.value_of("BROKERS"),
-                      m.value_of("TOPIC"),
-                      m.is_present("FROMBEG"),
-                      convert,
-                      count)
-        }
-        _ => {
-            run_byte(pqrs_decoder,
-                     matches.value_of("INPUT"),
-                     string_to_stream_type(matches.value_of("STREAM").unwrap_or("single")),
-                     convert,
-                     count)
-        }
+        ("kafka", Some(m)) => run_kafka(m),
+        _ => run_byte(&matches),
     }
 }
 
-fn run_kafka(decoder: PqrsDecoder,
-             brokers: Option<&str>,
-             topic: Option<&str>,
-             from_beginning: bool,
-             convert: Option<&str>,
-             count: i32) {
-    if let (Some(brokers), Some(topic)) = (brokers, topic) {
+fn run_kafka(matches: &ArgMatches) {
+    if let (Some(brokers), Some(topic)) = (matches.value_of("BROKERS"), matches.value_of("TOPIC")) {
         match StreamConsumer::for_kafka(String::from(brokers),
                                         String::from(topic),
-                                        from_beginning) {
-            Ok(x) => decode_or_convert(decoder, x, convert, count),
+                                        matches.is_present("FROMBEG")) {
+            Ok(x) => decode_or_convert(x, matches),
             Err(e) => errexit!(e),
         }
     } else {
@@ -93,13 +68,9 @@ fn run_kafka(decoder: PqrsDecoder,
     }
 }
 
-fn run_byte(decoder: PqrsDecoder,
-            input: Option<&str>,
-            stream: StreamType,
-            convert: Option<&str>,
-            count: i32) {
+fn run_byte(matches: &ArgMatches) {
     let stdin = io::stdin();
-    let mut input: Box<Read> = match input {
+    let mut input: Box<Read> = match matches.value_of("INPUT") {
         Some(x) => {
             let file = match File::open(&x) {
                 Ok(x) => x,
@@ -115,20 +86,20 @@ fn run_byte(decoder: PqrsDecoder,
             Box::new(stdin.lock())
         }
     };
-    decode_or_convert(decoder,
-                      StreamConsumer::for_byte(stream, &mut input),
-                      convert,
-                      count);
+    decode_or_convert(StreamConsumer::for_byte(string_to_stream_type(matches
+                                                                         .value_of("STREAM")
+                                                                         .unwrap_or("single")),
+                                               &mut input),
+                      matches);
 }
 
-fn decode_or_convert(decoder: PqrsDecoder,
-                     consumer: StreamConsumer,
-                     convert: Option<&str>,
-                     count: i32) {
+fn decode_or_convert(consumer: StreamConsumer, matches: &ArgMatches) {
+    let count = value_t!(matches, "COUNT", i32).unwrap_or(-1);
+
     let stdout = io::stdout();
     let out_is_tty = unsafe { libc::isatty(libc::STDOUT_FILENO) != 0 };
 
-    if let Some(ref convert_type) = convert {
+    if let Some(ref convert_type) = matches.value_of("CONVERT") {
         let converter = StreamConverter::new(consumer, string_to_stream_type(convert_type));
         let stdout_ = &mut stdout.lock();
         for (ctr, item) in converter.enumerate() {
@@ -140,6 +111,11 @@ fn decode_or_convert(decoder: PqrsDecoder,
             stdout_.write_all(&item).unwrap();
         }
     } else {
+        let decoder = match PqrsDecoder::new(matches.value_of("MSGTYPE")) {
+            Ok(x) => x,
+            Err(e) => errexit!(e),
+        };
+
         for (ctr, item) in consumer.enumerate() {
             if count >= 0 {
                 if ctr >= count as usize {
