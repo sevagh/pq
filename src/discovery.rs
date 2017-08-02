69b16bc3bff8a25e784b2be238a7de57a6862b89
Sevag Hanssian <sevag.hanssian@gmail.com>
2017-08-02 09:03:29 -0400

    Add /etc/pq as another location to check for fdset files

--- src/discovery.rs
+++ src/discovery.rs
@@ -8,8 +8,8 @@ use protobuf::descriptor::FileDescriptorSet;
 use errors::*;
 
 pub fn get_loaded_descriptors() -> Result<Vec<FileDescriptorSet>> {
-    let (fdsets, fdset_path) = match discover_fdsets() {
-        Ok((fdsets, fdsets_path)) => (fdsets, fdsets_path),
+    let fdsets = match discover_fdsets() {
+        Ok(fdsets) => fdsets,
         Err(e) => return Err(e),
     };
     let mut descriptors: Vec<FileDescriptorSet> = Vec::new();
@@ -25,41 +25,47 @@ pub fn get_loaded_descriptors() -> Result<Vec<FileDescriptorSet>> {
     }
 
     if descriptors.is_empty() {
-        return Err(format!("no valid fdset files in {}", fdset_path).into());
+        return Err(
+            "no valid fdset files found in dirs: $FDSET_PATH, $HOME/.pq, /etc/pq".into(),
+        );
     }
     Ok(descriptors)
 }
 
-fn discover_fdsets() -> Result<(Vec<PathBuf>, String)> {
+fn discover_fdsets() -> Result<Vec<PathBuf>> {
     let mut fdset_files = Vec::new();
 
-    let path = match env::var("FDSET_PATH") {
-        Ok(x) => PathBuf::from(x),
-        Err(_) => {
-            let mut home = match env::home_dir() {
-                Some(x) => x,
-                None => return Err("$HOME is not defined".into()),
-            };
-            home.push(".pq");
-            home
-        }
-    };
+    if let Ok(x) = env::var("FDSET_PATH") {
+        let p = PathBuf::from(x);
+        fdset_files.append(&mut get_fdset_files_from_path(&p));
+    }
 
-    let path_str = path.to_string_lossy().into_owned();
+    if let Some(mut x) = env::home_dir() {
+        x.push(".pq");
+        fdset_files.append(&mut get_fdset_files_from_path(&x));
+    }
 
-    match read_dir(path.as_path()) {
-        Ok(paths) => {
-            for p in paths {
-                let path = p.expect("error iterating through paths").path();
-                if !path.is_dir() {
-                    fdset_files.push(path);
-                }
+    let x = PathBuf::from("/etc/pq");
+    fdset_files.append(&mut get_fdset_files_from_path(&x));
+
+    if fdset_files.is_empty() {
+        return Err(
+            "No fdset files found. Dirs checked: $FDSET_PATH, $HOME/.pq, /etc/pq".into(),
+        );
+    }
+
+    Ok(fdset_files)
+}
+
+fn get_fdset_files_from_path(path: &PathBuf) -> Vec<PathBuf> {
+    let mut ret = vec![];
+    if let Ok(paths) = read_dir(path.as_path()) {
+        for p in paths {
+            let path = p.expect("error iterating through paths").path();
+            if !path.is_dir() {
+                ret.push(path);
             }
         }
-        Err(_) => return Err(format!("Path {} not found", path_str).into()),
     }
-    if fdset_files.is_empty() {
-        return Err(format!("No valid fdset files in path {}", path_str).into());
-    }
-    Ok((fdset_files, path_str))
+    ret
 }
