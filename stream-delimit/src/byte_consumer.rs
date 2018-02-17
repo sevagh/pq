fc3836af76472f334a24c5ed86809d08bd419d79
Vincent Foley <vfoley@gmail.com>
2018-02-17 16:24:45 -0500

    Make ByteConsumer own its reader
    This is in preparation for making pq multi-threaded; the trait `Read`
    is not `Send` and so with the previous declaration (read: &'a mut
    Read) ByteConsumer was _not_ Send and could not be moved into a
    closure, i.e., with thread::spawn.
    
    This commit modifies the API of ByteConsumer by making it take
    ownership of a value of type T where T implements the Read trait.

--- stream-delimit/src/byte_consumer.rs
+++ stream-delimit/src/byte_consumer.rs
@@ -5,24 +5,24 @@ use stream::*;
 use std::io::Read;
 
 /// A consumer for a byte stream
-pub struct ByteConsumer<'a> {
-    read: &'a mut Read,
+pub struct ByteConsumer<T: Read> {
+    read: T,
     type_: StreamType,
 }
 
-impl<'a> ByteConsumer<'a> {
+impl <T: Read> ByteConsumer<T> {
     /// Return a ByteConsumer from for single messages, varint or leb128-delimited
-    pub fn new(read: &'a mut Read, type_: StreamType) -> ByteConsumer {
+    pub fn new(read: T, type_: StreamType) -> ByteConsumer<T> {
         ByteConsumer { read, type_ }
     }
 }
 
-impl<'a> Iterator for ByteConsumer<'a> {
+impl <T: Read> Iterator for ByteConsumer<T> {
     type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Vec<u8>> {
         match self.type_ {
-            StreamType::Leb128 | StreamType::Varint => consume_single_varint(self.read),
+            StreamType::Leb128 | StreamType::Varint => consume_single_varint(&mut self.read),
             StreamType::Single => {
                 let ret: Option<Vec<u8>>;
                 let mut buf = Vec::new();
